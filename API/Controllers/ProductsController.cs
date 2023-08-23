c10f47ce00f3199cd3be87c52be4ce5347b51a0c
Milo Kastablank <31402027+SquallyWally@users.noreply.github.com>
2023-08-24 00:41:33 +0200

    Added EF

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    [HttpGet]
+    public string GetProducts() => "this will be a list of products";
+    
+    [HttpGet("{id:int}")]
+    public string GetProduct(int id) => "this will be a product";
+}
---
+++ API/Entities/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Entities;
+
+public class Product
+{
+    public int Id { get; init; }
+
+    public string Name { get; init; }
+}
