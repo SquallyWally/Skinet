aec531cf0323a31feff7d61e5b8e4ed4d5086212
Milo Kastablank <31402027+SquallyWally@users.noreply.github.com>
2023-08-24 22:07:29 +0200

    created BaseEntity and ProductBrand+Type

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -13,4 +13,8 @@ public class StoreContext : DbContext
     }
 
     public DbSet<Product> Products { get; set; }
+
+    public DbSet<ProductBrand> ProductBrands { get; set; }
+
+    public DbSet<ProductType> ProductTypes { get; set; }
 }
