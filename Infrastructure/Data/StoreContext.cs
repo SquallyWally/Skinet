c3d2cf9a4d936449cbae8a825f4644c44fc0b74c
Milo Kastablank <31402027+SquallyWally@users.noreply.github.com>
2023-08-24 22:41:18 +0200

    Added ProductConfiguration.cs

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -1,4 +1,6 @@
-﻿using Core.Entities;
+﻿using System.Reflection;
+
+using Core.Entities;
 
 using Microsoft.EntityFrameworkCore;
 
@@ -17,4 +19,11 @@ public class StoreContext : DbContext
     public DbSet<ProductBrand> ProductBrands { get; set; }
 
     public DbSet<ProductType> ProductTypes { get; set; }
+
+    protected override void OnModelCreating(
+        ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
 }
