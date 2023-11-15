597518afbba43f2a8fd913f8aefedcdc8dc604ca
Milo Kastablank <31402027+SquallyWally@users.noreply.github.com>
2023-11-15 22:53:13 +0100

    added search component

--- client/src/app/shared/models/shopParams.ts
+++ client/src/app/shared/models/shopParams.ts
@@ -2,6 +2,7 @@ export class ShopParams {
   brandId = 0;
   typeId = 0;
   sort = 'name';
+  search = '';
   pageNumber = 1;
   pageSize = 6;
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -23,6 +23,7 @@ export class ShopService {
     params = params.append('sort', shopParams.sort);
     params = params.append('pageNumber', shopParams.pageNumber);
     params = params.append('pageSize', shopParams.pageSize);
+    if (shopParams.search) params = params.append('search', shopParams.search);
 
     return this.http.get<Pagination<Product[]>>(this.baseUrl + 'products', {
       params,
