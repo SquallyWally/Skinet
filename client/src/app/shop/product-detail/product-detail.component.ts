1e8f1314f8f146a3f0f96da0d07d49f47d839b4a
Milo Kastablank <31402027+SquallyWally@users.noreply.github.com>
2023-11-16 00:46:18 +0100

    added product-detail page

--- client/src/app/shop/product-detail/product-detail.component.ts
+++ client/src/app/shop/product-detail/product-detail.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  styleUrls: ['./product-detail.component.scss'],
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
+  product?: Product;
 
+  /**
+   *
+   */
+  constructor(
+    private shopService: ShopService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (id)
+      this.shopService.getProduct(+id).subscribe({
+        // '+' symbol is used for casting
+        next: (product) => (this.product = product),
+        error: (error) => console.log(error),
+      });
+  }
 }
--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -29,6 +29,11 @@ export class ShopService {
       params,
     });
   }
+
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   getBrands() {
     return this.http.get<Brand[]>(this.baseUrl + 'products/brands');
   }
