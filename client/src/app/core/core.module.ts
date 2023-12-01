a6c025f271318521db6d16924732d6726132319f
Milo Kastablank <31402027+SquallyWally@users.noreply.github.com>
2023-12-01 12:16:02 +0100

    added ngx-spinner + loading

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { ServerErrorComponent } from './server-error/server-error.component';
 import { ToastrModule } from 'ngx-toastr';
 import { SectionHeaderComponent } from './section-header/section-header.component';
 import { BreadcrumbModule } from 'xng-breadcrumb';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { BreadcrumbModule } from 'xng-breadcrumb';
       preventDuplicates: true,
     }),
     BreadcrumbModule,
+    NgxSpinnerModule,
   ],
-  exports: [NavBarComponent, SectionHeaderComponent],
+  exports: [NavBarComponent, SectionHeaderComponent, NgxSpinnerModule],
 })
 export class CoreModule {}
