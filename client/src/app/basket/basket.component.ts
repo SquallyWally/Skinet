4d9128c89030802d6e1a82b6d8a571f04e846ae3
Milo Kastablank <31402027+SquallyWally@users.noreply.github.com>
2023-12-06 17:55:41 +0100

    styled basket page

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { BasketService } from './basket.service';
 
 @Component({
   selector: 'app-basket',
   templateUrl: './basket.component.html',
-  styleUrls: ['./basket.component.scss']
+  styleUrls: ['./basket.component.scss'],
 })
 export class BasketComponent {
-
+  /**
+   *
+   */
+  constructor(public basketService: BasketService) {}
 }
