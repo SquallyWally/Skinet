a47536dd3d6e9fee0c544c4bb5583e13924ae0a4
Milo Kastablank <31402027+SquallyWally@users.noreply.github.com>
2023-12-12 23:41:33 +0100

    persisting login

--- client/src/app/account/account.service.ts
+++ client/src/app/account/account.service.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject, map } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
 import { User } from '../shared/models/user';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
@@ -14,6 +14,19 @@ export class AccountService {
   currentUser$ = this.currentUserSource.asObservable();
 
   constructor(private http: HttpClient, private router: Router) {}
+
+  loadCurrentUser(token: string) {
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Bearer ${token}`);
+
+    return this.http.get<User>(`${this.baseUrl}account`, { headers }).pipe(
+      map((user) => {
+        localStorage.setItem('token', user.token);
+        this.currentUserSource.next(user);
+        return user;
+      })
+    );
+  }
 
   login(values: any) {
     return this.http.post<User>(`${this.baseUrl}account/login`, values).pipe(
